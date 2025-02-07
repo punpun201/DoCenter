d746257ffbc0ffe1abeb05449da741ff54c42f4e
punpun201 <edgarguzar04@gmail.com>
2025-02-07 08:48:14 -0600

    Conexión a base de datos
    cambios en el settings.py para la conexión de la base de datos

--- gestion_docentes/settings.py
+++ gestion_docentes/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gestion_docentes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'control_docente',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
